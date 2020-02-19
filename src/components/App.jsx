82522693616aabd13e673b8f069b9ef0ad3ae818
thebantz <nicoledbanta@gmail.com>
2020-02-18 18:10:55 -0800

    Successfully render the app view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+// TODO: Render the `App` component to the DOM
+// import VideoList from './components/VideoList.js';
+// import VideoListEntry from './components/VideoListEntry.js';
+// import VideoPlayer from './components/VideoPlayer.js';import Search from './components/Search.js';
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -16,6 +22,32 @@ var App = () => (
   </div>
 );
 
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render () {
+//     return (
+//       <div>
+//         <nav className="navbar">
+//           <div className="col-md-6 offset-md-3">
+//             <div><h5><em>search</em>  </h5></div>
+//           </div>
+//         </nav>
+//         <div className="row">
+//           <div className="col-md-7">
+//             <div><h5><em>videoPlayer</em>  </h5></div>
+//           </div>
+//           <div className="col-md-5">
+//             <div><h5><em>videoList</em> VideoList </h5></div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
+console.log('hello');
+
+ReactDOM.render(<App />, document.getElementById('app'));
